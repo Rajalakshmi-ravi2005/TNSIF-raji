2d5ae692bcedd164e1c792616df4e40bce7688f0
RAJALAKSHMI R <RAJALAKSHMI R@DESKTOP-NTEK7KD>
2025-09-29 16:06:20 +0530

    This is a basic Java Project

---
+++ Tnsf_java_raji/src/Assignment/java/StudentsDemo.java
@@ -0,0 +1,22 @@
+package Assignment.java;
+
+public class StudentsDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Students s1 = new Students();
+		s1.setRollno(101);
+		s1.setAge(19);
+		s1.setName("Raj");
+		
+		System.out.println(s1);
+		
+		Students s2 = new Students();
+		s2.setRollno(102);
+		s2.setName("Vijay");
+		s2.setAge(19);
+		
+		System.out.println(s1);
+	}
+
+}
---
+++ Tnsf_java_raji/src/basic/java/AdditionalScanner.java
@@ -0,0 +1,17 @@
+package basic.java;
+import java.util.*;
+
+public class AdditionalScanner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc= new Scanner(System.in);
+		System.out.println("enter the number:");
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = a+b;
+		System.out.println(c);
+	}
+
+}
+
